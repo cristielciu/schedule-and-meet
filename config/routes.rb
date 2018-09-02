d65c815161ec82d988cbf072471f26db1048589f
Cristi Ielciu <ielciu.cristi@gmail.com>
2018-09-02 19:35:23 +0300

    Edit appointment + nr of participants

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resource :appointment, only: [:new, :create, :show]
+  resource :appointment, only: [:new, :create, :show, :edit, :update]
   resource :answer, only: [:create, :update]
 
   get '/' => "welcome#index", as: :root
