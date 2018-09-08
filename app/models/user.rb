0b316db2240caccc327609814d1f33acd9d0bbae
Cristi Ielciu <ielciu.cristi@gmail.com>
2018-09-08 16:07:28 +0300

    Display app ordered

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :appointments, foreign_key: :creator_id
 
   def future_appointments
-    appointments.in_the_future
+    appointments.in_the_future.order(:starts_at, :name)
   end
 end
